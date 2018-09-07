7eb17de86a80e5b90833ed4ff7f148cf3b698168
liuheng <liuheng0315@gmail.com>
2018-09-07 08:09:18 +0800

    机器学习练习三，onevsall，一对多逻辑回归

--- machineLearning_oneVsAll.py
+++ machineLearning_oneVsAll.py
@@ -27,3 +27,33 @@ for y,cla in enumerate(classes):
         if i==0:
             plt.title(cla)
 plt.show()
+
+#正则化逻辑回归算法
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def out(x,w):
+    return sigmoid(np.dot(x,w))
+
+def f(param,*args):
+    X_train,y_train,reg=args
+    m,n=X_train.shape
+    J=0
+    theta=param.reshape((n,1))
+    h=out(X_train,theta)
+    theta_1=theta[1:,:]
+    J=-1*np.sum(y_train*np.log(h)+(1-y_train)*np.log(1-h))/m+reg*(theta_1.T.dot(theta_1))/(2*m)
+    return J
+
+def gradf(params,*args):
+    X_train,y_train,reg=args
+    m,n=X_train.shape
+    theta=params.reshape(-1,1)
+    h=out(X_train,theta)
+    grad=np.zeros((X_train.shape[1],1))
+    theta_1=theta[1:,:]
+    grad=X_train.T.dot((h-y_train))/m
+    grad[1:,:]+=reg*theta_1/m
+    g=grad.ravel()
+    return g
+
